be955ffb30eb465825d4d28217a7e5baa681db28
Rakesh Ghosal <rakeshghosal.nsec@gmail.com>
2023-10-11 20:00:04 +0530

    Updated the readme file

--- src/test/java/org/example/pageObjects/LoginPage.java
+++ src/test/java/org/example/pageObjects/LoginPage.java
@@ -26,7 +26,6 @@ public class LoginPage {
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
-
     }
 
     public String getErrorMessage() {
